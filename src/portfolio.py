ec703f2bf02eb4fe3e68b00c392aac47783c14a6
Guilherme Barreto <gbarreto@quantm3.com>
2024-12-02 18:48:12 -0300

    add new prompts

--- src/portfolio.py
+++ src/portfolio.py
@@ -30,27 +30,11 @@ def load_tickers():
     return data
 
 
-def build_portfolio(tickers: List[dict] = None):
-    if tickers is None:
-        tickers = load_tickers()
-
-    # building portfolio model
-    symbols = [l["symbol"] for l in tickers]
-    fields = {s: float for s in symbols}
-    description = {
-        l["symbol"]: f"portfolio weight in {l['companyName']}" for l in tickers
-    }
-    Portfolio = create_model("Portfolio", fields, description)
-
-    class DummyChatOpenAI(ChatOpenAI):
-        def __init__(self, **kwargs):
-            super(ChatOpenAI, self).__init__()
-
-        def __call__(self, *args, **kwargs):
-            output = Portfolio(**{k: 0 for k in fields}).model_dump().__repr__()
-            output = output.replace("'", '"')
-            content = f"```\n{output}\n```"
-            return AIMessage(content)
+def model_1(llm, tickers: List[dict], parser):
+    """
+    - one-shot
+    - uses pydantic_object to format output
+    """
 
     # asks llm
     values = {"date": dt.datetime.now().strftime("%Y-%m-%d")}
@@ -60,16 +44,14 @@ def build_portfolio(tickers: List[dict] = None):
     {format_instructions}
     """
 
-    parser = PydanticOutputParser(pydantic_object=Portfolio)
-    llm = ChatOpenAI(
-        openai_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL, temperature=0
-    )
     message = HumanMessagePromptTemplate.from_template(template=PROMPT)
     chat_prompt = ChatPromptTemplate.from_messages([message])
     chat_prompt_with_values = chat_prompt.format_prompt(
         prompt_template=prompt_template,
         format_instructions=parser.get_format_instructions(),
     )
+
+    # send prompt to llm
     output = llm(chat_prompt_with_values.to_messages())
 
     enc = tiktoken.encoding_for_model(OPENAI_MODEL)
@@ -94,13 +76,88 @@ def build_portfolio(tickers: List[dict] = None):
     output = {}
     output["date"] = dt.datetime.now().strftime("%Y-%m-%d")
     output["execution_ts"] = ts
-    output["name"] = "openai_fundamental"
+    output["name"] = "model_1"
     output["portfolio"] = portfolio
+    upload_json(data=[output], path="database/model_portfolio.json", extend=True)
+    return None
 
+
+def model_2(llm, tickers: List[dict], parser):
+    """
+    - one-shot
+    - asks the model to return a json-object
+    """
+    symbols = [l["symbol"] for l in tickers]
+    values = {"date": dt.datetime.now().strftime("%Y-%m-%d"), "stock_list": symbols}
+    prompt_template = load_jinja_prompt("prompts/per-market-fundamental.jinja", values)
+    PROMPT = """{prompt_template}"""
+
+    # send prompt to llm
+    output = llm(PROMPT)
+
+    enc = tiktoken.encoding_for_model(OPENAI_MODEL)
+    tokens = enc.encode(PROMPT)
+
+    # dump raw
+    ts = get_current_timestamp()
+
+    raw_output = (
+        {"prompt": PROMPT}
+        | output.model_dump()
+        | {"prompt_hash": hash_string(PROMPT)}
+        | {"token_size": len(tokens)}
+        | {"execution_ts": ts}
+    )
+    upload_json(data=[raw_output], path="database/model_dump.json", extend=True)
+
+    # dump parsed
+    portfolio = parser.parse(output.content)
+    portfolio = portfolio.model_dump()
+
+    output = {}
+    output["date"] = dt.datetime.now().strftime("%Y-%m-%d")
+    output["execution_ts"] = ts
+    output["name"] = "model_1"
+    output["portfolio"] = portfolio
     upload_json(data=[output], path="database/model_portfolio.json", extend=True)
 
     return None
 
 
+def model_3():
+    """
+    - two-shots (first asks a general overview, then asks the portfolio)
+    """
+    return None
+
+
+def run(tickers: dict):
+    # building portfolio model
+    symbols = [l["symbol"] for l in tickers]
+    fields = {s: float for s in symbols}
+    description = {
+        l["symbol"]: f"portfolio weight in {l['companyName']}" for l in tickers
+    }
+    Portfolio = create_model("Portfolio", fields, description)
+    parser = PydanticOutputParser(pydantic_object=Portfolio)
+
+    class DummyChatOpenAI(ChatOpenAI):
+        def __init__(self, **kwargs):
+            super(ChatOpenAI, self).__init__()
+
+        def __call__(self, *args, **kwargs):
+            output = Portfolio(**{k: 0 for k in fields}).model_dump().__repr__()
+            output = output.replace("'", '"')
+            content = f"```\n{output}\n```"
+            return AIMessage(content)
+
+    llm = ChatOpenAI(
+        openai_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL, temperature=0
+    )
+    model_1(llm, tickers, parser)
+
+    return None
+
+
 if __name__ == "__main__":
-    build_portfolio()
+    run()
